61758328f0432b965df8956c9f1722428b2a86f3
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-21 19:05:44 +0100

    3-5 Accessing query string parameters

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 interface Props {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 }
 
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>divProductPage: {slug}</div>;
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <div>
+      divProductPage: {slug} {sortOrder}
+    </div>
+  );
 };
 
 export default ProductPage;
