35c5a97436aa9f965d1a638f2f277ec411a3ddb8
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-22 07:37:10 +0100

    3-9-Showing Loading UIs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,5 @@
 // "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 import Link from "next/link";
 
@@ -16,7 +16,9 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
       <Link href="/users/new" className="btn">
         New User
       </Link>
-      <UserTable sortOrder={sortOrder} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </>
   );
 };
