cfb575f36bcff1e73fe287a8a0c551691cc03899
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-22 07:17:00 +0100

    3-8-Programmatic Navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,7 @@
+// "use client";
 import React from "react";
 import UserTable from "./UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
@@ -11,6 +13,9 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </>
   );
