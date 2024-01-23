9e18c45c1652e7fb739f4ab0f84668b0ab989888
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-23 21:00:05 +0100

    4-5-Updating an object

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface Props {
+  params: { id: number };
+}
+
+// function to GET a single user
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch data from a db
+  // If not found, return 404 error
+  // Else return data
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json([{ id: 1, name: "Mosh" }]);
+}
+
+// function to update a single user
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // read the req body and validate the req body
+  const body = await request.json();
+  // If invalid, return 400 (bad request)
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // Fetch the user with the give id
+  // If doesn't exist (in the db), return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Update the user (in db)
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
