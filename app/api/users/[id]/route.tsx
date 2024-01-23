c1fc1dae05aa1872b44487a5983cb9cc7f1338bf
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-23 21:09:29 +0100

    4-6-Deleting an object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
@@ -35,4 +36,19 @@ export async function PUT(
   // Update the user (in db)
   // Return the updated user
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+// function to delete a user
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch user from db
+  // If not found, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Delete the user
+  // Return 200
+  return NextResponse.json({});
+  //   above, you can return the deleted obj or an empty obj;
 }
