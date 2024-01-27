e406dcf20d158dbf0f530840e71637343bb5b290
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-27 12:47:42 +0100

    8-5-Styling Emails

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Body,
@@ -6,20 +6,31 @@ import {
   Link,
   Preview,
   Container,
+  Tailwind,
 } from "@react-email/components";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello world!</Text>
-          <Link href="htttps://codewithmosh.com">www.codewithmosh.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello world! {name}</Text>
+            <Link href="htttps://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
 };
 
 export default WelcomeTemplate;
