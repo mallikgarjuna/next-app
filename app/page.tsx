e65ca75256672677424ad644c5188b6e08ef4656
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-01-28 14:04:41 +0100

    9-4-Using fonts

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,7 @@ import coffee from "@/public/images/coffee.jpeg";
 export default async function Home() {
   return (
     <main className="relative h-screen">
-      <Image
-        src="https://bit.ly/react-cover"
-        alt="Coffee"
-        fill
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 780px) 50vw, 33vw"
-        quality={100}
-        priority
-      />
+      <h1>Hello World</h1>
     </main>
   );
 }
